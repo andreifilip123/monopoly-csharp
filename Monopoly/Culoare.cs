c0f515193fde1bd753a69b6e94d3bde1f4021ce5
Andrei Filip <filip@graffino.com>
2018-01-31 12:30:45 +0200

    Add class for Color

---
+++ Monopoly/Culoare.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Monopoly
+{
+    public class Culoare
+    {
+        public string culoare { get; set; }
+        public List<Proprietate> proprietati { get; set; }
+        private int numarProprietati { get; set; }
+        private Image image { get; set; }
+        
+        public Culoare(string culoare)
+        {
+            this.culoare = culoare;
+            proprietati = new List<Proprietate>();
+        }
+
+        public void adaugaProprietate(Proprietate proprietate)
+        {
+            proprietati.Add(proprietate);
+        }
+    }
+}
