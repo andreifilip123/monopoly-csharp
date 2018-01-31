ec0e0c6738ce692bbcaa23e88c26cf887c88ec60
Andrei Filip <filip@graffino.com>
2018-01-31 12:31:21 +0200

    Create form to show that player is in Jail

---
+++ Monopoly/EstiInInchisoare.Designer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Monopoly
+{
+    partial class EstiInInchisoare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent(Jucator jucator)
+        {
+            this.SuspendLayout();
+            // 
+            // EstiInInchisoare
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "EstiInInchisoare";
+            this.ResumeLayout(false);
+
+        }
+
+        private void platesc_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button platesc;
+        private System.Windows.Forms.Button dauCuZarul;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label numarIncercari;
+    }
+}
---
+++ Monopoly/EstiInInchisoare.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monopoly
+{
+    public partial class EstiInInchisoare : Form
+    {
+        Jucator jucatorInInchisoare;
+
+        public EstiInInchisoare(Jucator jucator)
+        {
+            InitializeComponent(jucator);
+            jucatorInInchisoare = jucator;
+            jucatorInInchisoare.eInInchisoare = true;
+        }
+
+        private void dauCuZarul_Click(object sender, EventArgs e)
+        {
+            if(jucatorInInchisoare.tureInInchisoare < 3)
+            {
+                Zar zar = new Zar();
+                if (zar.zar1 == zar.zar2)
+                {
+                    jucatorInInchisoare.eInInchisoare = false;
+                }
+                else
+                {
+                    jucatorInInchisoare.tureInInchisoare++;
+                }
+            } else
+            {
+                MessageBox.Show("Ai stat deja 3 ture in inchisoare. Trebuie sa platesti 50");
+                jucatorInInchisoare.bani -= 50;
+                jucatorInInchisoare.eInInchisoare = false;
+            }
+        }
+    }
+}
