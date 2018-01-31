f636727ed9f15ca5fe5b9c1e412a672b65f92f35
Andrei Filip <filip@graffino.com>
2018-01-31 12:25:45 +0200

    Add property class

---
+++ Monopoly/Proprietate.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+
+namespace Monopoly
+{
+    public abstract class Proprietate : Celula
+    {
+        public int valoareaProprietatii { get; set; }
+        public bool esteIpotecata { get; set; }
+        public string culoare { get; set; }
+        public Jucator proprietar { get; set; }
+
+        [JsonConstructor]
+        public Proprietate(int ID, string nume, int valoareaProprietatii, string culoare, string imagineaProprietatii) : base(ID, nume, imagineaProprietatii)
+        {
+            this.valoareaProprietatii = valoareaProprietatii;
+            this.culoare = culoare;
+        }
+
+        public Proprietate()
+        {
+
+        }
+
+        public abstract int chirie(Jucator jucator);
+
+        public override void activeaza(Jucator jucator)
+        {
+            return;
+        }
+    }
+}
