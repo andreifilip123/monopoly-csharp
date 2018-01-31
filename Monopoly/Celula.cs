1eafbf62fd2d6761774d226b66cb0a4928e0eff9
Andrei Filip <filip@graffino.com>
2018-01-31 12:22:53 +0200

    Add Cell and CellsList

---
+++ Monopoly/Celula.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Monopoly.Properties;
+using Newtonsoft.Json;
+
+namespace Monopoly
+{
+    public abstract class Celula : Button
+    {
+        public int ID { get; set; }
+        public string nume { get; set; }
+        public Image imagine { get; set; }
+
+        public abstract void activeaza(Jucator jucator);
+
+        [JsonConstructor]
+        public Celula(int ID, string nume, string imagine)
+        {
+            this.ID = ID;
+            this.nume = nume;
+            this.imagine = (Image)Resources.ResourceManager.GetObject(imagine);
+            Click += Celula_Click;
+        }
+
+        public Celula()
+        {
+            //ListaCelule.Instanta.Add(this);
+        }
+
+        private void Celula_Click(object sender, EventArgs e)
+        {
+            new AfisareCelula(imagine);
+        }
+
+        public static Comparison<Celula> comparare = (x, y) => x.ID.CompareTo(y.ID);
+    }
+}
---
+++ Monopoly/ListaCelule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class ListaCelule : List<Celula>
+    {
+        public static ListaCelule Instanta
+        {
+            get
+            {
+                if(lista != null)
+                {
+                    return lista;
+                } else
+                {
+                    lista = new ListaCelule();
+                    return lista;
+                }
+            }
+        }
+        private static ListaCelule lista;
+
+        private ListaCelule()
+        {
+
+        }
+    }
+}
