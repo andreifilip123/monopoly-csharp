de2293778e5458e31507f5bafded508144671583
Andrei Filip <filip@graffino.com>
2018-01-31 12:31:40 +0200

    Create Jail class

---
+++ Monopoly/Inchisoare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monopoly
+{
+    class Inchisoare : Celula
+    {
+        public Inchisoare(int ID, string nume, string imagine) : base(ID, nume, imagine)
+        {
+        }
+
+        public Inchisoare()
+        {
+            ListaCelule.Instanta.Add(this);
+        }
+
+        public override void activeaza(Jucator jucator)
+        {
+            return;
+        }
+    }
+}
