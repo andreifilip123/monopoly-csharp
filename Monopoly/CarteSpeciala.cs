68b459c3b8a1bc40f4e0613f54d462e697c8ae95
Andrei Filip <filip@graffino.com>
2018-01-31 12:28:26 +0200

    Add special cards positions and class

---
+++ Monopoly/CarteSpeciala.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class CarteSpeciala : Celula
+    {
+        public int ID { get; set; }
+        public string nume { get; set; }
+
+        [JsonConstructor]
+        public CarteSpeciala(int ID, string nume, string imagine) : base(ID, nume, imagine)
+        {
+
+        }
+
+        public CarteSpeciala()
+        {
+            ListaCelule.Instanta.Add(this);
+        }
+
+        public override void activeaza(Jucator jucator)
+        {
+            if(nume == "Sansa")
+            {
+                Sansa tempSansa = ListaSanse.Instanta[0];
+                Sansa.executa(tempSansa, jucator);
+                ListaSanse.Instanta.Remove(tempSansa);
+            } else
+            {
+                CutiaComunitatii tempCutiaComunitatii = ListaCutiaComunitatii.Instanta[0];
+                CutiaComunitatii.executa(tempCutiaComunitatii, jucator);
+                ListaCutiaComunitatii.Instanta.Remove(tempCutiaComunitatii);
+            }
+        }
+    }
+}
